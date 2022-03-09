ad46702ee681963eb563a0b26414b570e74fe7ad
Jonas Asafe <92129728+Linsjxl@users.noreply.github.com>
2022-03-09 15:43:35 -0300

    Add files via upload

---
+++ Desafio4.py
@@ -0,0 +1,12 @@
+"""
+Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""
+nota1= int(input("Insira a sua nota do primeiro bimestre: "))
+nota2= int(input("Insira a sua nota do segundo bimestre: "))
+nota3= int(input("Insira a sua nota do terceiro bimestre: "))
+nota4= int(input("Insira a sua nota do quarto bimestre: "))
+
+notaGeral = nota1 + nota2 + nota3 + nota4 
+media= notaGeral/4
+
+print(f"A sua media geral do ano foi {media}")
