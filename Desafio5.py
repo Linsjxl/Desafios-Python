3b7f1e1c12e21e3d09c673fd88a38cb7d9fa6ebf
Jonas Asafe <92129728+Linsjxl@users.noreply.github.com>
2022-03-09 15:45:57 -0300

    Delete Desafio5.py

--- Desafio5.py
+++
@@ -1,8 +0,0 @@
-mbs = int(input("Informe qual é o tamanho em Mb do arquivo que você deseja baixar: "))
-mbps = int(input("Agora informe em mbps a velocidade atual aproximada da sua internet: "))
-
-mbpsReal = mbps/8
-
-tempo = round(mbs/mbpsReal)
-
-print(f"O seu arquivo levará em média {tempo} segundos para ser baixado.")
