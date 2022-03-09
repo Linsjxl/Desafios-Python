3409ee2dba2969e1189f35d4e9520aba1a1de4cf
Jonas Asafe <92129728+Linsjxl@users.noreply.github.com>
2022-03-09 15:52:32 -0300

    Update Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -5,6 +5,7 @@ cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é
 vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a
 quantidades de latas de tinta a serem compradas e o preço total.
 """
+
 import math
 
 
