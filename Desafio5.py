55ca26340b3fd7063c03551859a574b803b166de
Jonas Asafe <92129728+Linsjxl@users.noreply.github.com>
2022-03-09 15:53:58 -0300

    Update Desafio5.py

--- Desafio5.py
+++ Desafio5.py
@@ -3,6 +3,7 @@ Faça um programa que peça o tamanho de um arquivo para download (em
 MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o
 tempo aproximado de download do arquivo usando este link (em minutos).
 """
+
 mbs = int(input("Informe qual é o tamanho em Mb do arquivo que você deseja baixar: "))
 mbps = int(input("Agora informe em mbps a velocidade atual aproximada da sua internet: "))
 
